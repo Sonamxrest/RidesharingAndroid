d0dcafaab8d084d85fde2c5b3e267d6faa19b8e0
Sonamxrest <xresthasonam@gmail.com>
2021-09-28 22:35:05 +0545

    New Updates

--- app/src/main/java/com/example/goodride/Response/UserResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.goodride.Response
-
-import com.example.goodride.model.RegisterModel
-
-data class UserResponse (
-    var success:Boolean?=null,
-            var data:RegisterModel?=null
-    )
